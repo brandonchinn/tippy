90902c482ac0eed7eccd6cb99324506c8dabdc01
Brandon Chinn <brandon@quizlet.com>
2016-09-24 11:57:08 -0700

    update to add settings for okay, good, excellent service

--- tippy/SettingsViewController.swift
+++ tippy/SettingsViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+let TIP_SETTING_DEFAULT_KEY = "TipSettingDefault"
+let TIP_SETTING_EXCELLENT_VALUE_KEY = "TipSettingExcellentValue"
+let TIP_SETTING_GOOD_VALUE_KEY = "TipSettingGoodValue"
+let TIP_SETTING_OKAY_VALUE_KEY = "TipSettingOkayValue"
 
+enum TipPercentType: Int {
+    case Okay = 0
+    case Good = 1
+    case Excellent = 2
+}
+
+class SettingsViewController: UIViewController {
+    
+
+
+    @IBOutlet weak var excellentTipValueLabel: UILabel!
+    @IBOutlet weak var goodTipValueLabel: UILabel!
+    @IBOutlet weak var okayTipValueLabel: UILabel!
     @IBOutlet weak var defaultTipSegmentedControl: UISegmentedControl!
+    
+    var excellentTipValue = 20
+    var goodTipValue = 18
+    var okayTipValue = 15
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         defaultTipSegmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.white], for: .selected)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadSettings()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +46,70 @@ class SettingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    override func viewWillDisappear(_ animated: Bool) {
+        saveSettings()
+    }
+    
+    func loadSettings() {
+        let defaults = UserDefaults.standard
+        let defaultTipIndex = defaults.integer(forKey: TIP_SETTING_DEFAULT_KEY)
+        defaultTipSegmentedControl.selectedSegmentIndex = defaultTipIndex
+        
+        excellentTipValue = defaults.object(forKey: TIP_SETTING_EXCELLENT_VALUE_KEY) as! Int? ?? excellentTipValue
+        updateLabel(label: excellentTipValueLabel, value: excellentTipValue)
+        goodTipValue = defaults.object(forKey: TIP_SETTING_GOOD_VALUE_KEY) as! Int? ?? goodTipValue
+        updateLabel(label: goodTipValueLabel, value: goodTipValue)
+        okayTipValue = defaults.object(forKey: TIP_SETTING_OKAY_VALUE_KEY) as! Int? ?? okayTipValue
+        updateLabel(label: okayTipValueLabel, value: okayTipValue)
+    }
+    
+    func saveSettings() {
+        let defaults = UserDefaults.standard
+        defaults.set(defaultTipSegmentedControl.selectedSegmentIndex, forKey: TIP_SETTING_DEFAULT_KEY)
+        defaults.set(excellentTipValue, forKey: TIP_SETTING_EXCELLENT_VALUE_KEY)
+        defaults.set(goodTipValue, forKey: TIP_SETTING_GOOD_VALUE_KEY)
+        defaults.set(okayTipValue, forKey: TIP_SETTING_OKAY_VALUE_KEY)
+        defaults.synchronize()
+    }
+    
+    /*
+     * TODO: make this into a custom control
+     */
+    @IBAction func onPlusButtonClick(_ sender: UIButton) {
+        adjustTipValue(tag: sender.tag, offset: 1)
+    }
+    
+    @IBAction func onMinusButtonClick(_ sender: AnyObject) {
+        adjustTipValue(tag: sender.tag, offset: -1)
+    }
+    
+    func adjustTipValue(tag: Int, offset: Int) {
+        switch tag {
+        case TipPercentType.Excellent.rawValue:
+            excellentTipValue += offset
+            // must be >= goodTipValue
+            excellentTipValue = max(excellentTipValue, goodTipValue)
+            updateLabel(label: excellentTipValueLabel, value: excellentTipValue)
+        case TipPercentType.Good.rawValue:
+            goodTipValue += offset
+            // must be <= excellentTipValue and >= okayTipValue
+            goodTipValue = max(goodTipValue, okayTipValue)
+            goodTipValue = min(goodTipValue, excellentTipValue)
+            updateLabel(label: goodTipValueLabel, value: goodTipValue)
+        case TipPercentType.Okay.rawValue:
+            okayTipValue += offset
+            // must be <= goodTipValue
+            okayTipValue = min(okayTipValue, goodTipValue)
+            updateLabel(label: okayTipValueLabel, value: okayTipValue)
+        default:
+            break
+        }
+    }
+    
+    func updateLabel(label: UILabel, value: Int) {
+        label.text = String(format: "%d%%", value)
+    }
+    
     /*
     // MARK: - Navigation
 
--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -15,13 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var tipValueLabel: UILabel!
+    
+    var tipPercentages: [Int: Int] = [TipPercentType.Okay.rawValue: 15, TipPercentType.Good.rawValue: 18, TipPercentType.Excellent.rawValue: 20]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         tipSegmentedControl.setTitleTextAttributes([NSForegroundColorAttributeName:UIColor.white], for: .selected)
         
-        calculateTip(billValue: (Double(billField.text!) ?? 0))
         billField.becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadSettings()
+        calculateTip(billValue: (Double(billField.text!) ?? 0))
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,9 +35,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func loadSettings() {
+        let defaults = UserDefaults.standard
+        let defaultTipIndex = defaults.integer(forKey: TIP_SETTING_DEFAULT_KEY)
+        tipSegmentedControl.selectedSegmentIndex = defaultTipIndex
+        tipPercentages[TipPercentType.Excellent.rawValue] = defaults.object(forKey: TIP_SETTING_EXCELLENT_VALUE_KEY) as! Int? ?? tipPercentages[TipPercentType.Excellent.rawValue]
+        tipPercentages[TipPercentType.Good.rawValue] = defaults.object(forKey: TIP_SETTING_GOOD_VALUE_KEY) as! Int? ?? tipPercentages[TipPercentType.Good.rawValue]
+        tipPercentages[TipPercentType.Okay.rawValue] = defaults.object(forKey: TIP_SETTING_OKAY_VALUE_KEY) as! Int? ?? tipPercentages[TipPercentType.Okay.rawValue]
+        
+    }
+    
     func calculateTip(billValue: Double) {
-        let tipPercentages = [0.15, 0.18, 0.2]
-        let tipPercent = tipPercentages[tipSegmentedControl.selectedSegmentIndex]
+        let tipPercent = Double(tipPercentages[tipSegmentedControl.selectedSegmentIndex]!) / 100.0
         let tipValue = billValue * tipPercent
         let total = billValue + tipValue
         tipLabel.text = String(format: "Tip (%d%%):", Int(tipPercent * 100))
